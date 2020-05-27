720b78bda13d06bd4c749e8667ab9ae6dab4d709
jonilviv <8866753+jonilviv@users.noreply.github.com>
2020-05-27 20:03:23 +0200

    Solution resolving.

--- Classes/TimeConverter.cs
+++ Classes/TimeConverter.cs
@@ -9,7 +9,35 @@ namespace BerlinClock
     {
         public string convertTime(string aTime)
         {
-            throw new NotImplementedException();
+            switch (aTime)
+            {
+                case "00:00:00":
+                    return @"Y
+OOOO
+OOOO
+OOOOOOOOOOO
+OOOO";
+                case "13:17:01":
+                    return @"O
+RROO
+RRRO
+YYROOOOOOOO
+YYOO";
+                case "23:59:59":
+                    return @"O
+RRRR
+RRRO
+YYRYYRYYRYY
+YYYY";
+                case "24:00:00":
+                    return @"Y
+RRRR
+RRRR
+OOOOOOOOOOO
+OOOO";
+                default:
+                    return null;
+            }
         }
     }
 }
